9bfff0e4c46a054327108cadddec2a299337ce16
Leonard Martin <leonard.martin@gmail.com>
2018-02-15 17:19:27 +0000

    Build complete schema model
    Adds all the properties expected on the model

--- schema/establishment.js
+++ schema/establishment.js
@@ -1,10 +1,28 @@
-const { STRING } = require('sequelize');
+const { STRING, ENUM, DATE, BOOLEAN, TEXT } = require('sequelize');
 
 module.exports = db => {
 
   const Establishment = db.define('establishment', {
+
     id: { type: STRING, primaryKey: true },
-    name: STRING
+    name: { type: STRING, allowNull: false },
+    status: { type: ENUM('active', 'pending', 'inactive', 'expired', 'revoked'), defaultsTo: 'inactive' },
+    issued: DATE,
+    licenceNumber: STRING,
+
+    address: { type: STRING, allowNull: false },
+    postcode: { type: STRING, allowNull: false },
+    telephone: { type: STRING, allowNull: false },
+    email: { type: STRING, allowNull: false },
+
+    procedure: { type: BOOLEAN, defaultsTo: false },
+    breeding: { type: BOOLEAN, defaultsTo: false },
+    supplying: { type: BOOLEAN, defaultsTo: false },
+    killing: { type: BOOLEAN, defaultsTo: false },
+    rehomes: { type: BOOLEAN, defaultsTo: false },
+
+    conditions: TEXT
+
   });
 
   return Establishment;
