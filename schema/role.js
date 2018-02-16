7f26973dde4c67359f9dd7b48ab54c8d419eeaec
Leonard Martin <leonard.martin@gmail.com>
2018-02-16 15:22:27 +0000

    Add all available role types to role schema

--- schema/role.js
+++ schema/role.js
@@ -1,10 +1,19 @@
 const { UUID, UUIDV1, ENUM } = require('sequelize');
 
+const ROLES = ENUM(
+  'elh',
+  'nacwo',
+  'nvs',
+  'nio',
+  'ntco',
+  'holc'
+);
+
 module.exports = db => {
 
   const Role = db.define('role', {
     id: { type: UUID, defaultValue: UUIDV1, primaryKey: true },
-    type: ENUM('elh', 'nacwo')
+    type: ROLES
   });
 
   return Role;
