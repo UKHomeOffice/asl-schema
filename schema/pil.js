2248101bb093368273ce35cdc660c80439891b72
Leonard Martin <leonard.martin@gmail.com>
2018-05-17 13:43:13 +0100

    Fix PIL schema to create two way association between profile and PIL

--- schema/pil.js
+++ schema/pil.js
@@ -1,10 +1,10 @@
-const { STRING, ENUM, DATE, TEXT } = require('sequelize');
+const { STRING, ENUM, DATE, TEXT, UUID, UUIDV1 } = require('sequelize');
 
 module.exports = db => {
 
   const PIL = db.define('pil', {
+    id: { type: UUID, defaultValue: UUIDV1, primaryKey: true },
 
-    id: { type: STRING, primaryKey: true },
     migrated_id: STRING,
     status: { type: ENUM('active', 'pending', 'inactive', 'expired', 'revoked'), defaultsTo: 'inactive' },
     issueDate: DATE,
