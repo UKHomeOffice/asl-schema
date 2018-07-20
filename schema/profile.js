f0eae648a6365cc7f918c5e049439c959e761eaf
Joe Fitter <joefitter@hotmail.com>
2018-07-20 15:04:34 +0100

    Added searchAndCountAll method to Profile
    * Search will search combined first and last names
    * Include pil.licenceNumber
    * Include role.type - cast to TEXT for comparison

--- schema/profile.js
+++ schema/profile.js
@@ -1,3 +1,5 @@
+const { merge } = require('lodash');
+const { Op } = require('sequelize');
 const { UUID, UUIDV1, STRING, DATEONLY, TEXT } = require('sequelize');
 
 module.exports = db => {
@@ -25,6 +27,67 @@ module.exports = db => {
       }
     }
   });
+
+  Profile.searchAndCountAll = ({ search, ...rest }) => {
+
+    const searchFullName = search => {
+      search = search.split(' ');
+      if (search.length > 1) {
+        return {
+          [Op.and]: [
+            { firstName: { [Op.iLike]: `%${search[0]}` } },
+            { lastName: { [Op.iLike]: `${search[1]}%` } }
+          ]
+        };
+      }
+      return {
+        [Op.or]: [
+          { firstName: { [Op.iLike]: `%${search[0]}%` } },
+          { lastName: { [Op.iLike]: `%${search[0]}%` } }
+        ]
+      };
+    };
+
+    const where = search
+      ? {
+        [Op.or]: [
+          searchFullName(search),
+          { '$pil.licenceNumber$': { [Op.iLike]: `%${search}%` } },
+          // cast ENUM field to TEXT for search
+          db.where(db.cast(db.col('type'), 'TEXT'), { [Op.eq]: search })
+        ]
+      }
+      : {};
+
+    return Promise.all([
+      Profile.count(),
+      Profile.findAndCountAll(merge({
+        where,
+        include: [{
+          model: db.models.role,
+          duplicating: false,
+          attributes: ['type']
+        },
+        {
+          model: db.models.pil,
+          duplicating: false,
+          attributes: ['licenceNumber']
+        },
+        {
+          model: db.models.project,
+          duplicating: false,
+          attributes: ['title']
+        }],
+        order: [['lastName', 'ASC'], ['firstName', 'ASC']]
+      }, rest))
+    ])
+      .then(([ total, result ]) => {
+        return {
+          ...result,
+          total
+        };
+      });
+  };
 
   return Profile;
 
