f394ffecaa0075c2583ef5bebfb1141fb530a918
Leonard Martin <leonard.martin@gmail.com>
2018-07-31 16:16:59 +0100

    Rename username param to user

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,7 @@ module.exports = {
       host: process.env.DATABASE_HOST,
       database: process.env.DATABASE_NAME,
       port: process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USERNAME,
+      user: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD
     }
   }
