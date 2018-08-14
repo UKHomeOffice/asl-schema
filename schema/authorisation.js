b7588fd4f95e477edba1543470df9b33e565d083
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2018-08-14 16:43:10 +0100

    authorisation schema and tests

--- schema/authorisation.js
+++ schema/authorisation.js
@@ -4,6 +4,26 @@ class Authorisation extends BaseModel {
   static get tableName() {
     return 'authorisations';
   }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['type'],
+      properties: {
+        id: { type: 'string' },
+        type: {
+          type: 'string',
+          enum: ['killing', 'rehomes']
+        },
+        method: { type: 'string' },
+        description: { type: 'string' },
+        'created_at': { type: 'string' },
+        'updated_at': { type: 'string' },
+        establishmentId: { type: 'string' },
+        deleted: { type: 'string' }
+      }
+    };
+  }
 }
 
 module.exports = Authorisation;
---
+++ test/authorisation.js
@@ -0,0 +1,29 @@
+const assert = require('assert');
+const Authorisation = require('../schema/authorisation');
+
+describe('Authorisation', () => {
+  it('throws a validation error when required properties are missing', () => {
+    const badJson = {
+      method: 'Placement in sanctuary'
+    };
+    assert.throws(() => Authorisation.fromJson(badJson), 'ValidationError: type: is a required property');
+  });
+
+  it('throws a validation error when invalid enum values are provided', () => {
+    const badJson = {
+      type: 'petting'
+    };
+    assert.throws(
+      () => Authorisation.fromJson(badJson),
+      'ValidationError: type: should be equal to one of the allowed values'
+    );
+  });
+
+  it('successfully instantiates when given a valid schema', () => {
+    const goodJson = {
+      type: 'rehomes',
+      method: 'Placement in sanctuary'
+    };
+    assert.equal(typeof Authorisation.fromJson(goodJson), 'object');
+  });
+});
