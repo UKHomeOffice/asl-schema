bb25275ce1d3430d344c45d9db91a587ee73e574
Joe Fitter <joefitter@hotmail.com>
2018-08-23 11:06:06 +0100

    Database should return strings for dates fields, as this is what we are validating1 (#59)

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const { Model } = require('objection');
 const Knex = require('knex');
-
 const Schema = require('./schema');
+
+const types = require('pg').types;
+const moment = require('moment');
+
+const TIMESTAMPTZ_OID = 1184;
+const TIMESTAMP_OID = 1114;
+const DATE_OID = 1082;
+
+const parseFn = val => {
+  return val === null ? null : moment(val).toISOString();
+};
+
+types.setTypeParser(TIMESTAMPTZ_OID, parseFn);
+types.setTypeParser(TIMESTAMP_OID, parseFn);
+types.setTypeParser(DATE_OID, parseFn);
 
 module.exports = connection => {
 
