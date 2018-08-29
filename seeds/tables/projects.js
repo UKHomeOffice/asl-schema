66924c8d2c862a0d53a037d49ec0960ff44f054b
Elvin Ali <ali.elvin@gmail.com>
2018-08-29 17:48:32 +0100

    Add license numbers and populate project list

--- seeds/tables/projects.js
+++ seeds/tables/projects.js
@@ -1,17 +1,30 @@
-const { sample } = require('lodash');
+const { sample, sampleSize } = require('lodash');
 const projects = require('../data/projects.json');
 
 module.exports = {
   populate: knex => {
     return Promise.all(
       projects.map(project => {
+        return knex('profiles').then(profiles => {
+          return knex('projects').insert({
+            licenceHolderId: sample(profiles).id,
+            ...project
+          });
+        });
+      })
+    );
+  },
+  populateList: knex => {
+    const projectsList = sampleSize(projects, 5);
+    return Promise.all(
+      projectsList.map(project => {
         return knex('profiles')
-          .then(profiles => {
-            return knex('projects')
-              .insert({
-                licenceHolderId: sample(profiles).id,
-                ...project
-              });
+          .first()
+          .then(profile => {
+            return knex('projects').insert({
+              licenceHolderId: profile.id,
+              ...project
+            });
           });
       })
     );
