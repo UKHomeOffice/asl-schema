5e91fce69493832e97860f84fcbd2b422f6eb0ea
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2018-10-08 12:40:42 +0100

    use cleaner syntax

--- knexfile.js
+++ knexfile.js
@@ -5,33 +5,27 @@ try {
   require('dotenv').config();
 } catch (e) {}
 
-let test = {
-  client: 'postgresql',
-  connection: {
-    host: process.env.DATABASE_HOST || 'localhost',
-    database: process.env.DATABASE_NAME,
-    user: process.env.DATABASE_USERNAME || 'asl-test'
-  }
-};
-
-let development = {
-  client: 'postgresql',
-  connection: {
-    host: process.env.DATABASE_HOST,
-    database: process.env.DATABASE_NAME,
-    port: process.env.DATABASE_PORT,
-    user: process.env.DATABASE_USERNAME || 'postgres',
-    password: process.env.DATABASE_PASSWORD
-  }
-};
-
-// DO NOT enable SNAKE_MAPPER for migrations! @see https://github.com/tgriesser/knex/issues/2644
-if (process.env.SNAKE_MAPPER) {
-  test = { ...test, ...knexSnakeCaseMappers() };
-  development = { ...development, ...knexSnakeCaseMappers() };
-}
+const snakeCaseMapper = process.env.SNAKE_MAPPER ? knexSnakeCaseMappers() : {};
 
 module.exports = {
-  test,
-  development
+  test: {
+    ...snakeCaseMapper,
+    client: 'postgresql',
+    connection: {
+      host: process.env.DATABASE_HOST || 'localhost',
+      database: process.env.DATABASE_NAME,
+      user: process.env.DATABASE_USERNAME || 'asl-test'
+    }
+  },
+  development: {
+    ...snakeCaseMapper,
+    client: 'postgresql',
+    connection: {
+      host: process.env.DATABASE_HOST,
+      database: process.env.DATABASE_NAME,
+      port: process.env.DATABASE_PORT,
+      user: process.env.DATABASE_USERNAME || 'postgres',
+      password: process.env.DATABASE_PASSWORD
+    }
+  }
 };
