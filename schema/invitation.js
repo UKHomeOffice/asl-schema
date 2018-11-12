3b2a56255d74d69e8457298aa6af55e9102a5cfa
Leonard Martin <leonard.martin@gmail.com>
2018-11-12 11:02:17 +0000

    Add id property to invitation schema

--- schema/invitation.js
+++ schema/invitation.js
@@ -1,5 +1,6 @@
 const BaseModel = require('./base-model');
 const { externalPermissions } = require('@asl/constants');
+const { uuid } = require('../lib/regex-validation');
 
 class Invitation extends BaseModel {
   static get tableName() {
@@ -12,6 +13,7 @@ class Invitation extends BaseModel {
       required: ['role', 'establishmentId', 'email', 'token'],
       additionalProperties: false,
       properties: {
+        id: { type: 'string', pattern: uuid.v4 },
         token: { type: 'string' },
         role: { type: 'string', enum: externalPermissions },
         createdAt: { type: 'string', format: 'date-time' },
