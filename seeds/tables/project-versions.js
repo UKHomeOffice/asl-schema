a11e57825ac8707295c1c6cf3dfd3d1c0c1da9e9
Leonard Martin <leonard.martin@gmail.com>
2019-02-27 10:18:36 +0000

    Add comment explaining one-at-a-time insertion

--- seeds/tables/project-versions.js
+++ seeds/tables/project-versions.js
@@ -2,6 +2,7 @@ const projectVersions = require('../data/project-versions.json');
 
 module.exports = {
   populate: knex => {
+    // insert one at a time asyncly to force unique ordered creation timestamps
     return projectVersions
       .reverse()
       .reduce((p, projectVersion) => {
