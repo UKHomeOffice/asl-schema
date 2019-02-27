26fff4f633c27030b5e78b0d70b1014cf653ee70
Leonard Martin <leonard.martin@gmail.com>
2019-02-27 10:16:45 +0000

    Improve code formatting

--- seeds/tables/project-versions.js
+++ seeds/tables/project-versions.js
@@ -1,8 +1,12 @@
 const projectVersions = require('../data/project-versions.json');
 
 module.exports = {
-  populate: knex => projectVersions.reverse().reduce((p, projectVersion) =>
-    p.then(() => knex('projectVersions').insert(projectVersion))
-    , Promise.resolve()),
+  populate: knex => {
+    return projectVersions
+      .reverse()
+      .reduce((p, projectVersion) => {
+        return p.then(() => knex('projectVersions').insert(projectVersion));
+      }, Promise.resolve());
+  },
   delete: knex => knex('projectVersions').del()
 };
