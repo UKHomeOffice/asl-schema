1f11df8ce4004adf2f32dafe91b8283ebf4b3bdc
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2019-10-14 17:31:47 +0100

    add data migration in later PR

--- migrations/20191014160144_add_project_amended_date.js
+++ migrations/20191014160144_add_project_amended_date.js
@@ -1,18 +1,6 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.table('projects', table => table.dateTime('amended_date'))
-  .then(() => {
-    return knex.raw(`UPDATE projects p
-      SET amended_date = (
-          SELECT MAX(pv.updated_at)
-          FROM project_versions pv
-          WHERE pv.project_id = p.id
-          AND pv.status = 'granted'
-          GROUP BY pv.project_id
-          HAVING COUNT(*) > 1
-      )
-    `);
-  });;
+  return knex.schema.table('projects', table => table.dateTime('amended_date'));
 };
 
 exports.down = function(knex, Promise) {
