fc01c0e8bd6bfa6add1f44421cf375a37ed1de7b
Leonard Martin <leonard.martin@gmail.com>
2020-01-08 16:02:36 +0000

    Add billable query method to PIL model
    Allows querying for the billable PILs for a given year and given establishment.
    
    Adds some transfers to the seeds to allow for testing of billing queries.

--- schema/pil.js
+++ schema/pil.js
@@ -1,8 +1,73 @@
+const { QueryBuilder } = require('objection');
 const BaseModel = require('./base-model');
 const { pilStatuses } = require('@asl/constants');
 const { uuid } = require('../lib/regex-validation');
 
+class PILQueryBuilder extends QueryBuilder {
+
+  billable({ establishmentId, start, end }) {
+    return this
+      .select('pils.id', 'pils.licenceNumber', 'pils.establishmentId', 'pils.issueDate', 'pils.revocationDate')
+      .eager('[profile.establishments, pilTransfers]')
+      .leftJoinRelation('profile')
+      .where(builder => {
+        // PIL was active during billing period
+        builder
+          .where('issueDate', '<=', end)
+          .whereNotNull('issueDate')
+          .where(builder => {
+            builder
+              .whereNull('revocationDate')
+              .orWhere('revocationDate', '>', start);
+          });
+      })
+      .where(builder => {
+        builder
+          // PIL is currently held by establishment and has not been transferred in since end date
+          .where(builder => {
+            builder
+              .where({ 'establishmentId': establishmentId })
+              .whereNotExists(
+                PIL.relatedQuery('pilTransfers')
+                  .where('createdAt', '>', end)
+              );
+          })
+          // PIL was transferred in or out of the establishment during the billing period
+          .orWhere(builder => {
+            builder
+              .whereExists(
+                PIL.relatedQuery('pilTransfers')
+                  .where(builder => {
+                    builder
+                      .where('fromEstablishmentId', establishmentId)
+                      .orWhere('toEstablishmentId', establishmentId);
+                  })
+                  .whereBetween('createdAt', [start, end])
+              );
+          })
+          // the first PIL transfer after the end of the billing period was out of the establishment
+          .orWhere(builder => {
+            builder
+              .where(
+                establishmentId,
+                PIL.relatedQuery('pilTransfers')
+                  .select('fromEstablishmentId')
+                  .where('createdAt', '>', end)
+                  .orderBy('createdAt', 'asc')
+                  .limit(1)
+              );
+          });
+      });
+  }
+
+}
+
 class PIL extends BaseModel {
+
+  static get QueryBuilder() {
+    return PILQueryBuilder;
+  }
+
   static get tableName() {
     return 'pils';
   }
@@ -50,7 +115,7 @@ class PIL extends BaseModel {
           to: 'establishments.id'
         }
       },
-      transfers: {
+      pilTransfers: {
         relation: this.HasManyRelation,
         modelClass: `${__dirname}/pil-transfer`,
         join: {
