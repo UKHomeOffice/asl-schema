fbd74bb13ec3e2a6691364753ea8c7a58b6542a8
Leonard Martin <leonard.martin@gmail.com>
2020-01-17 14:25:26 +0000

    Add an existence check to permission migration
    It can't rely on the roles existing, so add a wrapper which looks for the roles before trying to add permissions.

--- migrations/20200117133624_fee_waiver_permissions.js
+++ migrations/20200117133624_fee_waiver_permissions.js
@@ -1,22 +1,36 @@
 
 exports.up = function(knex) {
-  return knex.raw(`
-    grant select on fee_waivers to publicapi;
-    grant select on fee_waivers to internalapi;
-    grant select on fee_waivers to workflow;
-    grant select on fee_waivers to permissions;
-    grant select on fee_waivers to notifications;
-    grant select,insert,update on fee_waivers to resolver;
-  `);
+  const grantIfExists = (perms, role) => {
+    return knex.raw(`SELECT 1 FROM pg_roles WHERE rolname='${role}'`)
+      .then(exists => {
+        if (exists.rowCount) {
+          return knex.raw(`grant ${perms} on fee_waivers to ${role}`)
+        }
+      });
+  }
+  return Promise.resolve()
+    .then(() => grantIfExists('select', 'publicapi'))
+    .then(() => grantIfExists('select', 'internalapi'))
+    .then(() => grantIfExists('select', 'workflow'))
+    .then(() => grantIfExists('select', 'notifications'))
+    .then(() => grantIfExists('select', 'permissions'))
+    .then(() => grantIfExists('select,insert,update', 'resolver'));
 };
 
 exports.down = function(knex) {
-  return knex.raw(`
-    grant select on fee_waivers to publicapi;
-    grant select on fee_waivers to internalapi;
-    grant select on fee_waivers to workflow;
-    grant select on fee_waivers to permissions;
-    grant select on fee_waivers to notifications;
-    grant select,insert,update on fee_waivers to resolver;
-  `);
+  const revokeIfExists = (perms, role) => {
+    return knex.raw(`SELECT 1 FROM pg_roles WHERE rolname='${role}'`)
+      .then(exists => {
+        if (exists.rowCount) {
+          return knex.raw(`revoke ${perms} on fee_waivers from ${role}`)
+        }
+      });
+  }
+  return Promise.resolve()
+    .then(() => revokeIfExists('select', 'publicapi'))
+    .then(() => revokeIfExists('select', 'internalapi'))
+    .then(() => revokeIfExists('select', 'workflow'))
+    .then(() => revokeIfExists('select', 'notifications'))
+    .then(() => revokeIfExists('select', 'permissions'))
+    .then(() => revokeIfExists('select,insert,update', 'resolver'));
 };
