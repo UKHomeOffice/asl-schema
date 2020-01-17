38b6b8e15e784de87fe38ea3376701029c39f96c
Leonard Martin <leonard.martin@gmail.com>
2020-01-17 14:42:23 +0000

    Create roles if they do not already exist before adding permissions

--- migrations/20200117133624_fee_waiver_permissions.js
+++ migrations/20200117133624_fee_waiver_permissions.js
@@ -1,36 +1,39 @@
 
 exports.up = function(knex) {
-  const grantIfExists = (perms, role) => {
+  const ensureRoles = (...roles) => {
+    return roles.reduce((promise, role) => promise.then(() => ensureRole(role)), Promise.resolve());
+  }
+  const ensureRole = (role) => {
     return knex.raw(`SELECT 1 FROM pg_roles WHERE rolname='${role}'`)
       .then(exists => {
-        if (exists.rowCount) {
-          return knex.raw(`grant ${perms} on fee_waivers to ${role}`)
+        if (exists.rowCount === 0) {
+          return knex.raw(`create role ${role}`);
         }
       });
   }
   return Promise.resolve()
-    .then(() => grantIfExists('select', 'publicapi'))
-    .then(() => grantIfExists('select', 'internalapi'))
-    .then(() => grantIfExists('select', 'workflow'))
-    .then(() => grantIfExists('select', 'notifications'))
-    .then(() => grantIfExists('select', 'permissions'))
-    .then(() => grantIfExists('select,insert,update', 'resolver'));
+    .then(() => ensureRoles(
+      'publicapi',
+      'internalapi',
+      'permissions',
+      'notifications',
+      'workflow',
+      'resolver'
+    ))
+    .then(() => knex.raw(`grant select on fee_waivers to publicapi`))
+    .then(() => knex.raw(`grant select on fee_waivers to internalapi`))
+    .then(() => knex.raw(`grant select on fee_waivers to permissions`))
+    .then(() => knex.raw(`grant select on fee_waivers to notifications`))
+    .then(() => knex.raw(`grant select on fee_waivers to workflow`))
+    .then(() => knex.raw(`grant select,insert,update on fee_waivers to resolver`));
 };
 
 exports.down = function(knex) {
-  const revokeIfExists = (perms, role) => {
-    return knex.raw(`SELECT 1 FROM pg_roles WHERE rolname='${role}'`)
-      .then(exists => {
-        if (exists.rowCount) {
-          return knex.raw(`revoke ${perms} on fee_waivers from ${role}`)
-        }
-      });
-  }
   return Promise.resolve()
-    .then(() => revokeIfExists('select', 'publicapi'))
-    .then(() => revokeIfExists('select', 'internalapi'))
-    .then(() => revokeIfExists('select', 'workflow'))
-    .then(() => revokeIfExists('select', 'notifications'))
-    .then(() => revokeIfExists('select', 'permissions'))
-    .then(() => revokeIfExists('select,insert,update', 'resolver'));
+    .then(() => knex.raw('revoke select on fee_waivers from publicapi'))
+    .then(() => knex.raw('revoke select on fee_waivers from internalapi'))
+    .then(() => knex.raw('revoke select on fee_waivers from workflow'))
+    .then(() => knex.raw('revoke select on fee_waivers from notifications'))
+    .then(() => knex.raw('revoke select on fee_waivers from permissions'))
+    .then(() => knex.raw('revoke select,insert,update on fee_waivers from resolver'));
 };
