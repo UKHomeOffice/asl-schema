a2726a807ac0e57d68c873234a2091550ee5abd1
Leonard Martin <leonard.martin@gmail.com>
2020-02-06 11:03:20 +0000

    Add test specific place seeds
    The tests for places pick random places to work with and hope that they don't conflict. Instead create specific places for each test case.

--- seeds/tables/places.js
+++ seeds/tables/places.js
@@ -1,4 +1,5 @@
-const { sample, mapValues } = require('lodash');
+const { sample, mapValues, sampleSize } = require('lodash');
+const { suitabilityCodes, holdingCodes } = require('@asl/constants');
 const places = require('../data/places.json');
 
 module.exports = {
@@ -11,6 +12,12 @@ module.exports = {
             .insert({
               nacwoId: place.nacwoId || sample(nacwos).id,
               ...mapValues(place, (val, key) => {
+                if (key === 'holding') {
+                  val = val || sampleSize(holdingCodes, 2);
+                }
+                if (key === 'suitability') {
+                  val = val || sampleSize(suitabilityCodes, 2);
+                }
                 if (key === 'holding' || key === 'suitability') {
                   return JSON.stringify(val);
                 }
