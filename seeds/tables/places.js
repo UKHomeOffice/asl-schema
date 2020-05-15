b465774b5e682dc06617194aed1e6cf14b10590d
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2020-05-15 13:00:35 +0100

    seed place roles table with nacwos

--- seeds/tables/places.js
+++ seeds/tables/places.js
@@ -1,19 +1,17 @@
-const { sample, mapValues, sampleSize } = require('lodash');
+const { sample, mapValues, sampleSize, omit, flatten, isEmpty, concat } = require('lodash');
 const { suitabilityCodes, holdingCodes } = require('@asl/constants');
 const places = require('../data/places.json');
 
 module.exports = {
   populate: knex => {
     return Promise.resolve()
-      .then(() => knex('roles').where('type', 'nacwo'))
-      .then(nacwos => {
+      .then(() => {
         return Promise.all(places.map(place => {
           place.holding = place.holding || sampleSize(holdingCodes, 2);
           place.suitability = place.suitability || sampleSize(suitabilityCodes, 2);
           return knex('places')
             .insert({
-              nacwoId: place.nacwoId || sample(nacwos).id,
-              ...mapValues(place, (val, key) => {
+              ...mapValues(omit(place, 'nacwos'), (val, key) => {
                 if (key === 'holding' || key === 'suitability') {
                   return JSON.stringify(val);
                 }
@@ -21,6 +19,34 @@ module.exports = {
               })
             });
         }));
+      })
+      .then(() => knex('places').select('id'))
+      .then(placeIds => {
+        placeIds = placeIds.map(p => p.id);
+
+        return knex('roles').where('type', 'nacwo').where('establishment_id', 8201)
+          .then(nacwos => {
+            const placesWithNacwosDefined = places.filter(place => place.nacwos !== undefined);
+            const placeIdsWithoutNacwosDefined = placeIds.filter(id => !placesWithNacwosDefined.find(p => p.id === id));
+
+            const seededPlaceRoles = flatten(placesWithNacwosDefined.map(place => {
+              if (Array.isArray(place.nacwos) && !isEmpty(place.nacwos)) {
+                return place.nacwos.map(nacwoRoleId => ({
+                  role_id: nacwoRoleId,
+                  place_id: place.id
+                }));
+              }
+            })).filter(Boolean);
+
+            const randomPlaceRoles = placeIdsWithoutNacwosDefined.map(id => {
+              return {
+                place_id: id,
+                role_id: sample(nacwos).id
+              };
+            });
+
+            return knex('place_roles').insert(concat(seededPlaceRoles, randomPlaceRoles));
+          });
       });
   },
   delete: knex => knex('places').del()
