4c1f951f29b2f52d1b6bad4da4a1c111a329d352
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2020-08-14 13:44:55 +0100

    knex migrations already use transactions

--- migrations/20200813142330_merge_establishment_10801_into_10261.js
+++ migrations/20200813142330_merge_establishment_10801_into_10261.js
@@ -4,9 +4,9 @@ const fromEstablishmentId = 10801;
 const toEstablishmentId = 10261;
 
 exports.up = function(knex) {
-  return knex.transaction(trx => merge(trx, fromEstablishmentId, toEstablishmentId));
+  return merge(knex, fromEstablishmentId, toEstablishmentId);
 };
 
 exports.down = function(knex) {
-  return knex.transaction(trx => revertMerge(trx, fromEstablishmentId, toEstablishmentId));
+  return revertMerge(knex, fromEstablishmentId, toEstablishmentId);
 };
