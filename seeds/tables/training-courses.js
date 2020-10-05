c688467213615a6e26c783945d1fbd0a826f89c9
Leonard Martin <leonard.martin@gmail.com>
2020-10-05 17:28:53 +0100

    Add `whereHasBillablePIL` query method to Profile model
    This allows requests to be made based on whether a person has any kind of billable PIL at an establishment, and get lists of people with billable PILs.

--- seeds/tables/training-courses.js
+++ seeds/tables/training-courses.js
@@ -1,7 +1,25 @@
+const { omit } = require('lodash');
 const trainingCourses = require('../data/training-courses.json');
 
 module.exports = {
   populate: knex => {
-    return knex('training_courses').insert(trainingCourses.map(t => ({ ...t, species: JSON.stringify(t.species) })));
+    return trainingCourses.reduce((promise, course) => {
+      return Promise.resolve()
+        .then(() => {
+          return knex('training_courses').insert({
+            ...omit(course, 'participants'),
+            species: JSON.stringify(course.species)
+          }).returning('id');
+        })
+        .then(([id]) => {
+          const participants = (course.participants || []).map(participant => {
+            return {
+              ...participant,
+              training_course_id: id
+            };
+          });
+          return knex('training_pils').insert(participants);
+        });
+    }, Promise.resolve());
   }
 };
