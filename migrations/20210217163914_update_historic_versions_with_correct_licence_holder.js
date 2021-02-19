70467225c1522a5706e754baa7028a4354064d2e
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2021-02-19 12:01:48 +0000

    fix historical licence holders

---
+++ migrations/20210217163914_update_historic_versions_with_correct_licence_holder.js
@@ -0,0 +1,66 @@
+
+exports.up = function(knex) {
+  const changeLicenceHolderQuery = () => knex('changelog')
+    .where({ model_type: 'project', action: 'update' })
+    .orderBy('updated_at', 'desc');
+
+  const previousHolderQuery = changeHolderEvent => knex('changelog')
+    .where({ model_type: 'project', model_id: changeHolderEvent.model_id })
+    .where('action', '!=', 'update')
+    .where('updated_at', '<', changeHolderEvent.updated_at)
+    .whereRaw(`state->>'licenceHolderId' != '${changeHolderEvent.state.licenceHolderId}'`)
+    .orderBy('updated_at', 'desc')
+    .first();
+
+  const updateProjectVersionHolderQuery = fix => knex('project_versions')
+    .where({ project_id: fix.projectId })
+    .where('updated_at', '<', fix.holderChangedAt)
+    .update({ licence_holder_id: fix.holderBefore });
+
+  let fixers = [];
+  let nopes = [];
+
+  return Promise.resolve()
+    .then(() => changeLicenceHolderQuery())
+    .then(changeHolderEvents => {
+      const promises = changeHolderEvents.map(changeHolderEvent => {
+        return previousHolderQuery(changeHolderEvent).then(previousHolderEvent => {
+          if (previousHolderEvent) {
+            fixers.push({
+              projectId: changeHolderEvent.model_id,
+              holderChangedAt: changeHolderEvent.updated_at,
+              holderBefore: previousHolderEvent.state.licenceHolderId,
+              holderAfter: changeHolderEvent.state.licenceHolderId
+            });
+          } else {
+            nopes.push({
+              projectId: changeHolderEvent.model_id,
+              holderChangedAt: changeHolderEvent.updated_at,
+              holderBefore: undefined,
+              holderAfter: changeHolderEvent.state.licenceHolderId
+            });
+          }
+        });
+      });
+
+      return Promise.all(promises);
+    })
+    .then(() => {
+      console.log(`found ${fixers.length} change of licence holder to fix`);
+      console.log(fixers);
+      console.log(`found ${nopes.length} change of licence holder we can't fix`);
+      console.log(nopes);
+
+      const promises = fixers.map(fix => {
+        const query = updateProjectVersionHolderQuery(fix);
+        console.log(query.toString());
+        return query;
+      });
+
+      return Promise.all(promises);
+    });
+};
+
+exports.down = function(knex) {
+  return Promise.resolve();
+};
