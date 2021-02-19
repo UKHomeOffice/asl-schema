be0131ae6e76cb723e7632e8f6461a8ee569c623
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2021-02-19 12:13:47 +0000

    rollback should reset to the current licence holder

--- migrations/20210217163914_update_historic_versions_with_correct_licence_holder.js
+++ migrations/20210217163914_update_historic_versions_with_correct_licence_holder.js
@@ -58,5 +58,11 @@ exports.up = function(knex) {
 };
 
 exports.down = function(knex) {
-  return Promise.resolve();
+  // revert back to the licence holder from the project
+  return knex.raw(`
+    UPDATE project_versions
+    SET licence_holder_id = projects.licence_holder_id
+    FROM projects
+    WHERE projects.id = project_versions.project_id
+  `);
 };
