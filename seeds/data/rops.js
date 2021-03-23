7621273ee3aa8e066a8414f3f246a1e7bd9f4163
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2021-03-23 12:31:05 +0000

    add fixtures for 2020 and 2021 rops

---
+++ seeds/data/rops.js
@@ -0,0 +1,37 @@
+module.exports = [
+  {
+    projectId: 'b978c34f-d3bf-4747-ab03-97e44e2a80d8',
+    year: '2020',
+    status: 'submitted'
+  },
+  {
+    projectId: '98876c9f-9917-4c3b-bb88-722b0bb5a351',
+    year: '2020',
+    status: 'submitted'
+  },
+  {
+    projectId: '8804dfc0-ae9b-4bfc-9343-200b845b9cb0',
+    year: '2020',
+    status: 'submitted'
+  },
+  {
+    projectId: 'db22f92c-0c2d-470f-9152-c203eb5d3262',
+    year: '2020',
+    status: 'submitted'
+  },
+  {
+    projectId: 'b87aa91e-65d8-4d38-9a70-305ba81c6109',
+    year: '2020',
+    status: 'draft'
+  },
+  {
+    projectId: '22436667-60d8-4afe-beaf-7284c1642e79',
+    year: '2021',
+    status: 'submitted'
+  },
+  {
+    projectId: '6ae2295c-dd09-4c30-bd19-c67dd7f24cea',
+    year: '2021',
+    status: 'draft'
+  }
+];
