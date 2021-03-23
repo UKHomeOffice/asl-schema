7bf8ff1005ba1aa360b541e403f391793616b8fa
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2021-03-23 12:31:33 +0000

    don't generate rops, just insert fixtures

--- seeds/tables/rops.js
+++ seeds/tables/rops.js
@@ -1,26 +1,6 @@
-const { sample } = require('lodash');
-const rops = require('../data/rops.json');
-
-// don't generate rops for these project ids
-const nopes = [];
+const rops = require('../data/rops');
 
 module.exports = {
-  populate: knex => {
-    return knex('projects')
-      .whereIn('status', ['expired', 'revoked'])
-      .then(projects => projects.filter(p => !nopes.includes(p.id)))
-      .then(projects => {
-        const generatedRops = projects.reduce((grops, project) => {
-          grops.push({
-            projectId: project.id,
-            year: new Date(project.status === 'expired' ? project.expiryDate : project.revocationDate).getFullYear(),
-            status: sample(['draft', 'submitted'])
-          });
-          return grops;
-        }, []);
-
-        return knex('rops').insert(rops.concat(generatedRops));
-      });
-  },
+  populate: knex => knex('rops').insert(rops),
   delete: knex => knex('rops').del()
 };
