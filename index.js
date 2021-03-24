94edd2fc2087d73e9a7fae42a2bd3dda793bada8
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2021-03-24 13:52:56 +0000

    cast int pg types to js numbers

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const moment = require('moment');
 const TIMESTAMPTZ_OID = 1184;
 const TIMESTAMP_OID = 1114;
 const DATE_OID = 1082;
+const INT4_OID = 23;
+const INT8_OID = 20;
 
 const parseFn = val => {
   return val === null ? null : moment(val).toISOString();
@@ -17,9 +19,17 @@ const dateParseFn = val => {
   return val === null ? null : moment(val).format('YYYY-MM-DD');
 };
 
+const intParseFn = val => {
+  return val === null ? null : parseInt(val, 10);
+};
+
 types.setTypeParser(TIMESTAMPTZ_OID, parseFn);
 types.setTypeParser(TIMESTAMP_OID, parseFn);
 types.setTypeParser(DATE_OID, dateParseFn);
+
+// WARNING: numbers larger than Number.MAX_SAFE_INTEGER will overflow and give different results
+types.setTypeParser(INT4_OID, intParseFn);
+types.setTypeParser(INT8_OID, intParseFn);
 
 module.exports = connection => {
 
