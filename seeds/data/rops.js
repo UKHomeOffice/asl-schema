0cc4a002ed40e25b84859e2cc07e4f04efaf761f
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2021-04-07 14:31:22 +0100

    add submitted date to submitted rops seeds

--- seeds/data/rops.js
+++ seeds/data/rops.js
@@ -2,22 +2,26 @@ module.exports = [
   {
     projectId: 'b978c34f-d3bf-4747-ab03-97e44e2a80d8',
     year: '2020',
-    status: 'submitted'
+    status: 'submitted',
+    submittedDate: '2021-01-10'
   },
   {
     projectId: '98876c9f-9917-4c3b-bb88-722b0bb5a351',
     year: '2020',
-    status: 'submitted'
+    status: 'submitted',
+    submittedDate: '2021-01-10'
   },
   {
     projectId: '8804dfc0-ae9b-4bfc-9343-200b845b9cb0',
     year: '2020',
-    status: 'submitted'
+    status: 'submitted',
+    submittedDate: '2021-01-10'
   },
   {
     projectId: 'db22f92c-0c2d-470f-9152-c203eb5d3262',
     year: '2020',
-    status: 'submitted'
+    status: 'submitted',
+    submittedDate: '2021-01-10'
   },
   {
     projectId: 'b87aa91e-65d8-4d38-9a70-305ba81c6109',
@@ -27,7 +31,8 @@ module.exports = [
   {
     projectId: '22436667-60d8-4afe-beaf-7284c1642e79',
     year: '2021',
-    status: 'submitted'
+    status: 'submitted',
+    submittedDate: '2021-01-10'
   },
   {
     projectId: '6ae2295c-dd09-4c30-bd19-c67dd7f24cea',
