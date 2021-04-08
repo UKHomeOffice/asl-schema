5b9291417dbff03bfd0065b003f9f46460474395
Leonard Martin <leonard.martin@gmail.com>
2021-04-08 12:51:09 +0100

    Set default properties on ROP seeds
    So they behave vaguely like real ROPs when accessed in the UI.

--- seeds/tables/rops.js
+++ seeds/tables/rops.js
@@ -1,6 +1,30 @@
 const rops = require('../data/rops');
 
+const defaults = {
+  basicSubpurposes: JSON.stringify([ 'oncology' ]),
+  endangered: false,
+  ga: false,
+  newGeneticLine: false,
+  nmbas: false,
+  otherSpecies: true,
+  placesOfBirth: JSON.stringify([ 'uk-licenced' ]),
+  postnatal: true,
+  proceduresCompleted: true,
+  productTesting: false,
+  purposes: JSON.stringify([ 'basic' ]),
+  reuse: false,
+  rodenticide: false,
+  scheduleTwoDetails: null,
+  species: JSON.stringify({ precoded: ['mice'], otherSpecies: [] })
+};
+
 module.exports = {
-  populate: knex => knex('rops').insert(rops),
+  populate: knex => knex('rops').insert(
+    rops.map(rop => ({
+      ...defaults,
+      submittedDate: rop.status === 'submitted' ? (new Date()).toISOString() : null,
+      ...rop
+    }))
+  ),
   delete: knex => knex('rops').del()
 };
