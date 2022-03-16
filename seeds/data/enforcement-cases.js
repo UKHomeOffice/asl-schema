a4cd172df50353f846613e45868c78aac7931f8f
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2022-03-16 16:06:46 +0000

    add seeds for enforcement

---
+++ seeds/data/enforcement-cases.js
@@ -0,0 +1,37 @@
+module.exports = [
+  {
+    // no flags
+    id: '870ce6f1-0b4f-404b-ac7a-214e76c801db',
+    caseNumber: '10010'
+  },
+  {
+    // single profile
+    id: 'e51bc9c2-f90e-413e-8d9a-55d9e26da22c',
+    caseNumber: '10020'
+  },
+  {
+    // single profile and pil
+    id: 'ab228908-110e-48fc-a6f3-222ecf6fb711',
+    caseNumber: '10030'
+  },
+  {
+    // single profile and multiple projects
+    id: '54a40457-4213-43b0-bca0-e8ae83c1b4aa',
+    caseNumber: '10040'
+  },
+  {
+    // single profile and multiple projects at different establishments
+    id: 'f44b7974-dbfb-443c-929e-bfcb07911787',
+    caseNumber: '10050'
+  },
+  {
+    // multiple profiles single project
+    id: 'feab4b6f-9480-46c4-a234-2f2ee551cc21',
+    caseNumber: '10060'
+  },
+  {
+    // PELH, approved areas, roles and establishment details
+    id: '796e926d-b7a9-4ce5-9702-5bef85f913b4',
+    caseNumber: '10070'
+  }
+];
