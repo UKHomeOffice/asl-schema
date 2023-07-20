eb720aa5e9171a0b6f7e7181d0e02a63758bc1d8
Ralph <ralph@ralph-cowling.com>
2023-07-20 10:42:45 +0200

    hbaTitle => hbaFilename
    We refer to it as the filename across the service so let's also do that in the db

--- migrations/20230719174206_add_hba_file_name_to_project_version.js
+++ migrations/20230719174206_add_hba_file_name_to_project_version.js
@@ -1,11 +1,11 @@
 exports.up = function (knex) {
   return knex.schema.table('project_versions', (table) => {
-    table.string('hba_name').nullable();
+    table.string('hba_filename').nullable();
   });
 };
 
 exports.down = function (knex) {
   return knex.schema.table('project_versions', (table) => {
-    table.dropColumn('hba_name');
+    table.dropColumn('hba_filename');
   });
 };
