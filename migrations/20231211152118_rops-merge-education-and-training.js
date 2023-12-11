36874a13f35eb2f37aa3943ee7b440d4fec278c2
Jeffrey Horton <jeff.horton@digital.homeoffice.gov.uk>
2023-12-11 16:38:53 +0000

    [ASL-4449] Fix linting

--- migrations/20231211152118_rops-merge-education-and-training.js
+++ migrations/20231211152118_rops-merge-education-and-training.js
@@ -1,19 +1,18 @@
 
-exports.up = async function(knex) {
-  return await knex.raw(
+exports.up = function(knex) {
+  return knex.raw(
     `UPDATE rops
 SET purposes = rops.purposes - '{education,training}'::text[] || '"education_or_training"'
 WHERE purposes @> '["education"]' OR purposes @> '["training"]';
 `
-  )
+  );
 };
 
-exports.down = async function(knex) {
-  return await knex.raw(
+exports.down = function(knex) {
+  return knex.raw(
     `UPDATE rops
 SET purposes = rops.purposes - '{education_or_training}'::text[] || '["education","training"]'::jsonb
 WHERE purposes @> '["education_or_training"]';
 `
-  )
+  );
 };
-
